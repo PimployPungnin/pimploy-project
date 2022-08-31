686fa42a3831b16de816a05d638f12eca4bf90a9
Tangmo <tangmo00pp@gmail.com>
2022-08-31 10:41:44 +0700

    make verify.php

---
+++ verify.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verify</title>
+</head>
+<body>
+    <h1><center>Webboard GGEZ</center></h1>
+    <hr>
+    <div align="center">
+        เข้าสู้ระบบด้วย <br>
+        Login = <?php echo $_POST["Login"];?> <br>
+        Password = <?php echo $_POST["Password"];?>
+</body>
+</html>
