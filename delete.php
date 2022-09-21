07f3a3b8b619522457345f1a8345d9a24efe1e1a
Tangmo <tangmo00pp@gmail.com>
2022-09-21 10:43:01 +0700

    add file

---
+++ delete.php
@@ -0,0 +1,9 @@
+<?php
+    session_start();
+    if($_SESSION['role']!='a'){
+        header("location:work2.php");
+        die()
+    }else{
+        echo "ลบกระทู้ หมายเลข $_GET[id]";
+    }
+?>
--- work2.php
+++ work2.php
@@ -46,14 +46,22 @@ session_start();
             <option value="study">--เรื่องเรียน--</option>
     </select>
     <div style="float:right">
+        <?php echo"ผู้ใช้งานระบบ : $_SESSION[username]";?>&nbsp;&nbsp;
         <a href="logout.php">ออกจากระบบ</a>
     </div>
+    <a href = "newpost.php">สร้างกระทู้ใหม่</a>
     <br>
-    <form action="post.php" method="get">
-    <?php 
-        for($i=1;$i<=10;$i++){
-            echo "<li><a href='post.php?id=$i'>กระทู้ $i</a></li>";
-        }
+    <ul>
+        <form action="post.php" method="get">
+        <?php 
+             for($i=1;$i<=10;$i++){
+                echo "<li>";
+                echo "<a href='post.php?id=$i'>กระทู้ที่ $i</a>";
+                if($_SESSION['role']=='a'){
+                    echo "&nbsp;&nbsp;<a href='delete.php?id=$_i'>ลบ</a>";
+                }
+                echo "</li>";
+            }
     ?>
     </form>
 </body>
